b2c1a7fdb9df47ab2cf854c25acf27e29472b612
essennejaye <sjeffers02@gmail.com>
2020-07-21 14:17:45 -0700

    html generated

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log('==============');
-        profileDataArr.forEach(profileItem => console.log(profileItem));
-    }
-};
-printProfileData(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log('==============');
+//         profileDataArr.forEach(profileItem => console.log(profileItem));
+//     }
+// };
+// printProfileData(profileDataArgs);
+
+
+
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
+
+    console.log('Portfolio complete! Checkout index.html to see the output!');
+});
