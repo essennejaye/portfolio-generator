0c185d6bbd316afb573bc9fc748a3c23a6c06eed
essennejaye <sjeffers02@gmail.com>
2020-07-21 17:38:54 -0700

    import npm

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
+// const fs = require('fs');
+
+// const generatePage = require('./src/page-template.js');
+
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
 
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log('==============');
-//         profileDataArr.forEach(profileItem => console.log(profileItem));
-//     }
-// };
-// printProfileData(profileDataArgs);
+// const [name, github] = profileDataArgs;
 
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw new Error (err);
+
+//     console.log('Portfolio complete! Checkout index.html to see the output!');
+// });
 
 
-const fs = require('fs');
 
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const [name, github] = profileDataArgs;
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
-
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-});
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
